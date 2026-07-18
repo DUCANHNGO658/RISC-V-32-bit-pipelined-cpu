7b9746e53fc2c73675955bc7ce587c9073e82643
Ngô Đức Anh <ducanhngo658@gmail.com>
2026-07-18 13:33:16 +0700

    updated

--- data_memory.v
+++ data_memory.v
@@ -13,7 +13,7 @@ reg [WIDTH-1:0] mem [1023:0];
 assign data_read = (memread==1'b1)? mem[addr>>2] : 32'b0;
 always@(posedge clk) begin
 if(rst) begin
-for(i = 0; i<1024; i= i+1) mem[i] <= 32'b0; end
+for(i = 0; i<1024; i= i+1) mem[i] = 32'b0; end
 else if(memwrite) mem[addr>>2] <= data_write;
 end
 endmodule
