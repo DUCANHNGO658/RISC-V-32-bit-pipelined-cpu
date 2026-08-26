d6350b69d1819f3b9f1084e6ac1d0441f8ed73da
Ngô Đức Anh <ducanhngo658@gmail.com>
2026-08-26 19:06:11 +0700

    updated

---
+++ pipeline_regs/IF_ID.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module IF_ID #(parameter WIDTH=32)(
+input wire clk,
+input wire rst,
+input wire jump,
+input wire pc_sel,
+input wire stall,
+input wire [WIDTH-1:0] inc_pc,inst,
+output reg [WIDTH-1:0] id_inc_pc, id_inst
+    );
+    
+always@(posedge clk) begin
+if(rst) begin
+id_inc_pc <= 32'b0;
+id_inst <=32'b0;
+end else  begin 
+if (!stall) begin 
+id_inc_pc <=inc_pc;
+id_inst <= (jump || pc_sel) ? 32'h0 : inst;
+end
+else begin
+id_inc_pc <=id_inc_pc;
+id_inst <=id_inst;
+end
+end
+end
+endmodule
